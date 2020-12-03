8108c249a5169ed0139fb86a1a0c9f4879c7799c
Allison Hsu <allison.hsu20@gmail.com>
2020-12-02 20:56:16 -0500

    added some functions

--- backend/index.ts
+++ backend/index.ts
@@ -22,27 +22,75 @@ app.get('/', (_, res) => res.send('Hello World!'));
 type User = {
   name: string;
   password: string;
-  groups: Array<Group>
+  groups: Array<Group>;
 };
 
-const Users = db.collection('Users');
+const users = db.collection('Users');
 
 type Group = {
   name: string;
   descr: string;
   url: string;
-  admin: typeof Users
-  users: typeof Users;
+  admin: number;
+  users: number;
 };
 
-const Groups = db.collection('Groups');
+const groups = db.collection('Groups');
 
-type Event = {
+type event = {
   name: string;
   descr: string;
-
+  url: string;
+  admin: number;
+  users: number;
 };
 
-const Events = db.collection('Events');
+const events = db.collection('Events');
+
+  // Add id field to our Post type
+  type UserWithID = User & {
+    id: string;
+  };
+  type GroupWithID = Group & {
+    id: string;
+  };
+  type EventWithID = event & {
+    id: string;
+  };
+
+  // create a user
+app.post('/createSong', function (req, res){
+  const user:User = req.body;
+  const myDoc = users.doc();
+  myDoc.set(user);
+  res.send(myDoc.id);
+});
+
+//read list of users
+app.get('/getUsers', async function (_, res) {
+  const sortedUsers = await users.orderBy('name', 'desc').get();
+  const userlist: UserWithID[] = [];
+  for (let doc of sortedUsers.docs) {
+    let user: UserWithID = doc.data() as UserWithID;
+    user.id = doc.id;
+    userlist.push(user);
+  }
+  res.send(users);
+});
+
+
+//get list of users in group
+//get list of events in groupp
+//add event to group
+//add user to group
+//update event 
+//update group
+//delete group
+//delete event
+
+
+
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
