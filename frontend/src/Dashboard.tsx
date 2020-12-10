1ef593bd3a2c26c0cb1906acb07c6c8727035836
zmindyli <zmindyli@gmail.com>
2020-12-09 23:57:40 -0500

    Imported Card API from mui, but width is wrong

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -1,18 +1,43 @@
 import React from 'react';
+import { Card, CardActionArea, CardActions, CardContent, Typography, withStyles, createStyles } from '@material-ui/core';
 
 type groupTileProps = {
   groupName: string,
   numEvents: number
 }
-const GroupTile = ({ groupName, numEvents }: groupTileProps) => {
+
+const styles = createStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+// TODO: tile is still page width
+const GroupTile = withStyles(styles)(({ groupName, numEvents }: groupTileProps) => {
   return (
-    <div>
-      {groupName} has {numEvents} upcoming {numEvents > 1 ? "events" : "event"}
-    </div>
+    // <div>
+    //   {groupName} has {numEvents} upcoming {numEvents > 1 ? "events" : "event"}
+    // </div>
+    <Card>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {groupName}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+
+      </CardActions>
+    </Card>
   );
-}
-// type dashboardProps accepts user as a parameter? 
-// displays all groups the user is part of
+});
+// TODO: make type dashboardProps 
+// accepts user as a parameter?, should display all groups the user is part of
 const Dashboard = () => {
   return (
     <GroupTile
