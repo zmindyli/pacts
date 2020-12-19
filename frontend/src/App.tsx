9c184b59242d179a742e9cde2d0ee651f5be753a
zmindyli <zmindyli@gmail.com>
2020-12-19 13:15:35 -0500

    Added switch statement so each group tile renders a different group page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,35 @@ import GroupPage from './GroupPage';
 
 function App() {
   const [currentPage, changePage] = useState(0); // pass state down to children
-  return (
-    <div>
-      {currentPage === 0 ? <Dashboard callback={(pageId: number) => changePage(pageId)} /> :
+  switch (currentPage) {
+    case 0:
+      return (
+        <Dashboard callback={(pageId: number) => changePage(pageId)} />);
+    case 1:
+      return (
         <GroupPage
-          name="Propel Incubator"
+          name="DTI"
           desc="Propel is an incubator by the Cornell Design and Tech Initiative that provides 
-        funding and connections for Cornell-focused student projects. 
-        It addresses the lack of available resources to student projects on campus. "
-          callback={(pageId: number) => changePage(pageId)} />}
-    </div>
-  );
+            funding and connections for Cornell-focused student projects. 
+            It addresses the lack of available resources to student projects on campus. "
+          callback={(pageId: number) => changePage(pageId)} />);
+    case 2:
+      return (
+        <GroupPage
+          name="Science Olympiad"
+          desc="Welcome to science"
+          callback={(pageId: number) => changePage(pageId)} />
+      )
+    case 3:
+      return (
+        <GroupPage
+          name="E.Motion"
+          desc="It's time to dance"
+          callback={(pageId: number) => changePage(pageId)} />
+      );
+    default:
+      return <Dashboard callback={(pageId: number) => changePage(pageId)} />;
+  }
 }
 
 export default App;
